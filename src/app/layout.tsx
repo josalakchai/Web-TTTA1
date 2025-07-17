4329836bae097eddfe8755f46668fdfc5944c7ce
Sitthisak <sitthisak2007.1@gmail.com>
2025-07-17 14:28:01 +0700

    Add Google Ads script to layout for ad integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6059901629514213"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
